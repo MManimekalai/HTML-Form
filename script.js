fb36cb25a61b3e83960cbf4356dbf31e89cc92b0
MManimekalai <124970075+MManimekalai@users.noreply.github.com>
2023-11-04 12:28:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,76 +1,56 @@
-var fname = document.getElementById("fname");
-var lname = document.getElementById("lname");
-var address1 = document.getElementById("address1");
-var address2 = document.getElementById("address2");
-var pin = document.getElementById("pin");
-var gender = document.getElementById("gender");
-var food = document.getElementById("food");
-var state = document.getElementById("state");
-var country = document.getElementById("country");
+var form = document.getElementById("myForm");
 
+form.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
-function tablecontent(fname,lname,address,pincode,gender,food,state,country){
-    var tbody = document.getElementById("tbody");
-    var tr = document.createElement("tr");
-    var td1 = document.createElement("td");
-    var td2 = document.createElement("td");
-    var td3 = document.createElement("td");
-    var td4 = document.createElement("td");
-    var td5 = document.createElement("td");
-    var td6 = document.createElement("td");
-    var td7 = document.createElement("td");
-    var td8 = document.createElement("td");
-    td1.innerHTML = fname;
-    td2.innerHTML = lname;
-    td3.innerHTML = address;
-    td4.innerHTML = pincode;
-    td5.innerHTML = gender;
-    td6.innerHTML = food;
-    td7.innerHTML = state;
-    td8.innerHTML = country;
-    tr.append(td1,td2,td3,td4,td5,td6,td7,td8);
-    tbody.append(tr);
-}
+    // Form validation
+    var firstName = fname.value.trim();
+    var lastName = lname.value.trim();
+    var addressLine1 = address1.value.trim();
+    var addressLine2 = address2.value.trim();
+    var pincode = pin.value.trim();
+    var genderElements = document.getElementsByName("gender");
+    var selectedGender = "";
+    var foodElements = document.getElementsByName("food");
+    var selectedFood = [];
+    var stateValue = state.value.trim();
+    var countryValue = country.value.trim();
 
-
-var submit=document.getElementById("submit");
-submit.addEventListener("click",function(event){
-    event.preventDefault();
-    var address=[];
-    address.push(address1.value);
-    address.push(address2.value);
-
-    var gender=document.getElementsByName("gender");
-    var gender1;
-    for(let i=0;i<gender.length;i++){
-        if(gender[i].checked){
-            gender1 =gender[i].value;
+    // Check if at least two food options are selected
+    for (var i = 0; i < foodElements.length; i++) {
+        if (foodElements[i].checked) {
+            selectedFood.push(foodElements[i].value);
         }
     }
-    
-    var food=document.getElementsByName("food");
-    var foodList=[];
-    var Count=0;
-    for(let i=0;i<food.length;i++){
-        if(food[i].checked){
-            foodList.push(food[i].value);
-            Count++;
-        }
-    }
-    if(foodList.length>=2){
-        value=foodList.join(", ");
-    }else{
-        value=alert("Choose atleast 2 options out of 5 in food");
-    }
 
-    tablecontent(fname.value,lname.value,address.join(", "),pin.value,gender1,value,state.value,country.value)
-    fname.value="";
-    lname.value="";
-    address1.value="";
-    address2.value="";
-    pin.value="";
-    gender.value="";
-    foodList=[];
-    state.value="";
-    country.value="";
-})
+    if (
+        firstName === "" ||
+        lastName === "" ||
+        addressLine1 === "" ||
+        pincode === "" ||
+        selectedGender === "" ||
+        selectedFood.length < 2 ||
+        stateValue === "" ||
+        countryValue === ""
+    ) {
+        alert("Please fill out all required fields and choose at least 2 food options.");
+    } else {
+        // If validation passes, add the data to the table
+        tablecontent(firstName, lastName, addressLine1 + ", " + addressLine2, pincode, selectedGender, selectedFood.join(", "), stateValue, countryValue);
+
+        // Clear form fields
+        fname.value = "";
+        lname.value = "";
+        address1.value = "";
+        address2.value = "";
+        pin.value = "";
+        for (var i = 0; i < genderElements.length; i++) {
+            genderElements[i].checked = false;
+        }
+        for (var i = 0; i < foodElements.length; i++) {
+            foodElements[i].checked = false;
+        }
+        state.value = "";
+        country.value = "";
+    }
+});
